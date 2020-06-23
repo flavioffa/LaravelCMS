52b6c74fd49a9d127e21bd564027c60e386cc1dd
Flavio Alves <flavioffagmail.com>
2020-06-23 18:48:09 -0300

    update

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -11,8 +11,8 @@
                 <div class="row align-items-center">
                     <div class="col-xl-7 col-md-6">
                         <div class="slider_text ">
-                            {{-- <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >{{$front_config['title']}}</h3> --}}
-                            {{-- <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">{{$front_config['subtitle']}}</p> --}}
+                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >{{$front_config['title']}}</h3>
+                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">{{$front_config['subtitle']}}</p>
                             <div class="video_service_btn wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">
                                 <a href="#" class="boxed-btn3">Get Start Now</a>
                             </div>
